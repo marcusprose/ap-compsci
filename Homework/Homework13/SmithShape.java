8cf420dc817961c1b98e6a5d4851fcdb63176fc4
Marcus Rose <marcusr2626@gmail.com>
2019-02-23 17:18:36 -0500

    Create SmithShape.java

---
+++ Homework/Homework13/SmithShape.java
@@ -0,0 +1,20 @@
+//Interface named in honor of Mr. Smith :)
+
+public interface SmithShape {
+
+	// The lower left x coordinate
+	public int getX();
+
+	// The lower left y coordinate
+	public int getY();
+
+	// The width of the shape
+	public int getWidth();
+
+	// The height of the shape
+	public int getHeight();
+
+	// The overall area of the shape
+	public double getArea();
+
+}
